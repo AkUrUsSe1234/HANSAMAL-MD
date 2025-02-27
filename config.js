83e6adbd93bd1df904d73f4668c04dd958e98f4e
AkUrUsSe1234 <akavindu467@gmail.com>
2025-02-28 04:07:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'HANSAMAL-MD=B84EHZ7B#t39ezuNsmCvU1IwqDuBvXPPcbtZvzteaUhr_i9dtvYk' : process.env.SESSION_ID,
 GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? '' : process.env.GITHUB_USER_NAME,
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '' : process.env.GITHUB_AUTH_TOKEN,        
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94711262551' : process.env.OWNER_NUMBER
